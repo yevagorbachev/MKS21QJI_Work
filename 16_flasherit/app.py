bfa3b76551916d34b58f8a2c7e2b96dfd5f67440
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2019-10-04 08:39:08 -0400

    changed header, added _base

--- 16_flasherit/app.py
+++ 16_flasherit/app.py
@@ -1,11 +1,13 @@
-#Yevgeniy Gorbachev
+#Real Actual Ukranians_Yevgeniy Gorbachev, Ivan Galakhov
 #SoftDev1 pd1
-#K<n> -- <K<n>.__name__>
-#ISO 8601 Date
+#K16 -- Oh yes, perhaps I do...
+#2019-10-07
 
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 parent = lambda str: str.rfind('/') if str.rfind('/') != -1 else str.rfind('\\') # finds parent directory from string
 __ppath__ = __file__[: parent(__file__)] # python file's parent directory
+
