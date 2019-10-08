2ff550db48a76a152ff1c2bc1f6aa97f8d979741
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2019-10-07 22:26:30 -0400

    finished

--- 17_csv2db/db_builder.py
+++ 17_csv2db/db_builder.py
@@ -1,4 +1,4 @@
-#Clyde "Thluffy" Sinclair
+#Clyde 'Thluffy' Sinclair
 #SoftDev  
 #skeleton :: SQLITE3 BASICS
 #Oct 2019
@@ -7,7 +7,7 @@ import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
 
 
-DB_FILE="discobandit.db"
+DB_FILE='discobandit.db'
 
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
@@ -15,14 +15,15 @@ c = db.cursor()               #facilitate db ops
 #==========================================================
 
 # < < < INSERT YOUR POPULATE-THE-DB CODE HERE > > >
+data = {}
+c.execute('CREATE TABLE roster (name TEXT, age INTEGER, id INTEGER PRIMARY KEY)')
 
-
-command = ""          # test SQL stmt in sqlite3 shell, save as string
-c.execute(command)    # run SQL statement
-
+with open('students.csv') as file:
+    data = csv.DictReader(file)
+    for row in data:
+        print(row)
+        c.execute('INSERT INTO roster (name, age, id) VALUES (\"{name}\", {age}, {id})'.format(name = row['name'], age = row['age'], id = row['id']))
 #==========================================================
 
 db.commit() #save changes
 db.close()  #close database
-
-
