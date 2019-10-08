56909d4978c25a6adeddeb4d5adc9e0de55a1b88
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2019-10-08 08:19:01 -0400

    finished now, for sure

--- 17_csv2db/db_builder.py
+++ 17_csv2db/db_builder.py
@@ -1,4 +1,4 @@
-#Clyde 'Thluffy' Sinclair
+#Krew_Yevgeniy Gorbachev, Tiffany Cao
 #SoftDev  
 #skeleton :: SQLITE3 BASICS
 #Oct 2019
@@ -18,12 +18,21 @@ c = db.cursor()               #facilitate db ops
 data = {}
 c.execute('CREATE TABLE roster (name TEXT, age INTEGER, id INTEGER PRIMARY KEY)')
 
-with open('students.csv') as file:
-    data = csv.DictReader(file)
+with open('students.csv') as csvfile0:
+    data = csv.DictReader(csvfile0)
     for row in data:
-        print(row)
         c.execute('INSERT INTO roster (name, age, id) VALUES (\"{name}\", {age}, {id})'.format(name = row['name'], age = row['age'], id = row['id']))
-#==========================================================
+
+
+data2 = {}
+c.execute("CREATE TABLE courses(code TEXT, mark INTEGER, id INTEGER)") #command executed
+
+with open('courses.csv') as csvfile1: #open and read courses.csv file and store as dictionary
+    data2 = csv.DictReader(csvfile1)
+    for row in data2: #for every row in reader, insert values into courses table
+        c.execute("INSERT INTO courses (code, mark, id) VALUES (\"{code}\", {mark}, {id})".format(code = row['code'], mark = row['mark'], id = row['mark']))
+
+#============================== ============================
 
 db.commit() #save changes
 db.close()  #close database
