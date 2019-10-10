da14bcd34c4d5dc71a56990bc961a2984c1168d3
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2019-10-10 08:32:33 -0400

    updated to python 3.7 formatting

--- 17_csv2db/db_builder.py
+++ 17_csv2db/db_builder.py
@@ -16,21 +16,23 @@ c = db.cursor()               #facilitate db ops
 
 # < < < INSERT YOUR POPULATE-THE-DB CODE HERE > > >
 data = {}
-c.execute('CREATE TABLE roster (name TEXT, age INTEGER, id INTEGER PRIMARY KEY)')
+c.execute('CREATE TABLE roster (name TEXT, age INTEGER, id INTEGER PRIMARY_KEY)')
 
 with open('students.csv') as csvfile0:
     data = csv.DictReader(csvfile0)
     for row in data:
-        c.execute('INSERT INTO roster (name, age, id) VALUES (\"{name}\", {age}, {id})'.format(name = row['name'], age = row['age'], id = row['id']))
+        name, age, id = row['name'], row['age'], row['id']
+        c.execute(f'INSERT INTO roster (name, age, id) VALUES (\'{name}\', {age}, {id})')
 
 
 data2 = {}
-c.execute("CREATE TABLE courses(code TEXT, mark INTEGER, id INTEGER)") #command executed
+c.execute('CREATE TABLE courses(code TEXT, mark INTEGER, id INTEGER PRIMARY_KEY)') #command executed
 
 with open('courses.csv') as csvfile1: #open and read courses.csv file and store as dictionary
     data2 = csv.DictReader(csvfile1)
     for row in data2: #for every row in reader, insert values into courses table
-        c.execute("INSERT INTO courses (code, mark, id) VALUES (\"{code}\", {mark}, {id})".format(code = row['code'], mark = row['mark'], id = row['mark']))
+        code, mark, id = row['code'], row['mark'], row['id']
+        c.execute(f'INSERT INTO courses (code, mark, id) VALUES (\'{code}\', {mark}, {id})')
 
 #============================== ============================
 
