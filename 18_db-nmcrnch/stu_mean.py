416ec46e71e697d014dbe3beb88d3cafc338392a
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2019-10-11 10:01:26 -0400

    tentatively finished 18

--- 18_db-nmcrnch/stu_mean.py
+++ 18_db-nmcrnch/stu_mean.py
@@ -7,8 +7,22 @@ c = db.cursor()
 
 c.execute('''SELECT roster.id, mark 
              FROM courses, roster 
-             WHERE courses.id = roster.id;''')
+             WHERE courses.id = roster.id''')
 grades_raw = c.fetchall()
-print(grades_raw)
+c.execute('''SELECT id
+             FROM roster''')
+ids = c.fetchall()
+grades_nclasses = {entry[0]:[0,0] for entry in ids}
+for entry in grades_raw:
+    grades_nclasses[entry[0]][0] += 1
+    grades_nclasses[entry[0]][1] += entry[1]
+print(grades_nclasses)
 
+c.execute('CREATE TABLE IF NOT EXISTS stu_avg (id INTEGER PRIMARY_KEY, gpa REAL)')
+for entry in grades_nclasses.items():
+    id = entry[0]
+    gpa = entry[1][1] / entry[1][0]
+    c.execute(f'INSERT INTO stu_avg (id, gpa) VALUES ({id}, {gpa})')
+
+db.commit()
 db.close()
