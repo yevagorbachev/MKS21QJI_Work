a196635e999d8a97f0a7219b7387edd232985079
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2019-10-15 16:25:28 -0400

    init wiki, unsure K#

---
+++ 21/app.py
@@ -0,0 +1,8 @@
+# SeQL_Yevgeniy Gorbachev, Ethan Joo, Eric Lau
+# SoftDev1 pd1
+# K21(?) -- ?*
+# 2019-10-??
+
+from flask import Flask, request, render_template, session, url_for
+
+app = Flask(__name__)
