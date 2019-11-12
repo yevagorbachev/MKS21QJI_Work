a85dbead850c8151080ad32a906a85e260b2e135
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2019-11-12 09:32:00 -0500

    added _base and index templates; image fetch working

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -1,11 +1,25 @@
 #Yevgeniy Gorbachev
 #SoftDev1 pd1
-#K<n> -- <K<n>.__name__>
-#ISO 8601 Date
+#K24 -- A RESTful Journey Skyward
+#2019-11-13
 
-from flask import Flask
+from flask import *
+import urllib.request
+import json
+from os import urandom
 
+__key__ = '5Vrh42prZ7Y2kxrO0KAH6fQ0CqpN5tDujnmZk8sF'
+__url__ = f'https://api.nasa.gov/planetary/apod?api_key={__key__}'
+
+app.secret_key = urandom(32)
 app = Flask(__name__)
 
-parent = lambda str: str.rfind('/') if str.rfind('/') != -1 else str.rfind('\\') # finds parent directory from string
-__ppath__ = __file__[: parent(__file__)] # python file's parent directory
+@app.route('/')
+def index():
+    request = urllib.request.urlopen(__url__)
+    response = request.read()
+    data = json.loads(response)
+    return render_template('index.html',img_url=data['url'])
+
+# if __name__ == 'main':
+app.run(debug=True)
