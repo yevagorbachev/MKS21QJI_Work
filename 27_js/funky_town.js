19d4cc819296fe6ef55902f50d9f7dffdf317bb5
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2019-12-11 08:41:52 -0500

    optimized fib

--- 27_js/funky_town.js
+++ 27_js/funky_town.js
@@ -3,12 +3,11 @@ console.log("Yeet");
 var fibarray = [0,1,1];
 
 var fibonacci = function(n) {
-	if (n < 3)
-		return 1;
-	if (fibarray[n])
+	if (fibarray[n]) {
 		return fibarray[n];
-	fibarray[n]=fibonacci(n-1)+fibonacci(n-2)
-	return fibarray[n]
+	}
+	fibarray[n]=fibonacci(n-1)+fibonacci(n-2);
+	return fibarray[n];
 };
 
 var gcd = function(a, b) {
@@ -30,13 +29,13 @@ console.log("Testing Fibonacci:");
 console.log(fibonacci(1));
 console.log(fibonacci(23));
 console.log(fibonacci(100));
-console.log("Testing GCD:");
-console.log(gcd(20, 5));
-console.log(gcd(1263, 343));
-console.log(gcd(24, 16));
-console.log("Testing Random Students:");
-console.log(randomStudent());
-console.log(randomStudent());
-console.log(randomStudent());
-console.log(randomStudent());
-console.log(randomStudent());
+// console.log("Testing GCD:");
+// console.log(gcd(20, 5));
+// console.log(gcd(1263, 343));
+// console.log(gcd(24, 16));
+// console.log("Testing Random Students:");
+// console.log(randomStudent());
+// console.log(randomStudent());
+// console.log(randomStudent());
+// console.log(randomStudent());
+// console.log(randomStudent());
