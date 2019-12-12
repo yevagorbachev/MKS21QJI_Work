62a7a264ef415512fb3a3ab49899b1a93686fdeb
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2019-12-11 19:18:09 -0500

    optimizations in js

--- 28_jsdom/funky_town.js
+++ 28_jsdom/funky_town.js
@@ -1,30 +1,32 @@
-//Amanda Zheng and Yvgeniy Gorbachev
+//Amanda Zheng and Yevgeniy Gorbachev
 //Softdev pd 1
 //K27 -- Sequential Progression
 //12-11-2019
 var fact = function(n){
-    if(n <= 1){
-      return 1;
+    if(n < 2){
+        return 1;
     }
-    return n*fact(n-1);
-};
+    return n * fact(n-1);
+}
 
-var fibarray = [0,1,1];
-var fib = function(n){
-    if (fibarray[n]){
-      return fibarray[n];
+var fib = function(n, prev = [0,1,1]){
+    if (prev[n]){
+        return prev[n];
     }
-    fibarray[n] = fib(n-1) + fib(n-2);
-    return fibarray[n];
-};
+    prev[n] = fib(n-1, prev) + fib(n-2, prev);
+    return prev[n];
+}
 
 var gcd = function(a,b){
     if(a % b == 0){
-      return b;
+        return b;
     }
     return gcd(b, a % b);
-};
+}
 
 var selectRandom = function(array){
+    if ((typeof array) == String) {
+        array = array.split(",")
+    }
     return array[Math.floor(Math.random()*array.length)];
-};
+}
